0bc003d51681f488e06e12f81333885ab161442f
Omerrizabahadir <omrbahadir@gmail.com>
2024-08-21 18:39:17 +0300

    Added diamond problem example

---
+++ src/methods/java_8/diamond_problem01/DiamondProblem.java
@@ -0,0 +1,9 @@
+package methods.java_8.diamond_problem01;
+
+public class DiamondProblem {
+
+    public static void main(String[] args) {
+        DiamondProblemTest diamondProblemTest = new DiamondProblemTest();
+        diamondProblemTest.defaultMethod();
+    }
+}
---
+++ src/methods/java_8/diamond_problem01/DiamondProblemTest.java
@@ -0,0 +1,14 @@
+package methods.java_8.diamond_problem01;
+
+public class DiamondProblemTest implements MyInterface01, MyInterface02{
+    @Override
+    public void test() {
+
+    }
+
+    //Diamond Problem'e neden old.için defaultMethod() 'u Override ettik
+    @Override
+    public void defaultMethod() {
+
+    }
+}
