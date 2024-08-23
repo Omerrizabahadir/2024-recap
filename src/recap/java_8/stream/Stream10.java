801a0bdf924cd64034c9e357839863e6416fc5d2
Omerrizabahadir <omrbahadir@gmail.com>
2024-08-23 22:03:05 +0300

    Java 8 Stream -> printing unique String

---
+++ src/recap/java_8/stream/Stream10.java
@@ -0,0 +1,19 @@
+package recap.java_8.stream;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
+        public class Stream10 {
+            public static void main(String[] args) {
+                String[] roseQuote = "a rose is a rose is a rose".split(" ");
+                Set<String> words = Arrays.stream(roseQuote).collect(Collectors.toSet());
+
+
+                Arrays.stream(roseQuote).collect(Collectors.toList());
+                Arrays.stream(roseQuote).toList();
+
+                System.out.println("Unique words (Set): " + words); // Sonuçları yazdırma
+            }
+        }
