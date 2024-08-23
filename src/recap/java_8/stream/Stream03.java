c5eae7113dd7a7dd1b844c180e7de3c066f5bd2d
Omerrizabahadir <omrbahadir@gmail.com>
2024-08-23 20:48:35 +0300

    Java 8 Stream -> using filter(), sorted(), findFirst()

---
+++ src/recap/java_8/stream/Stream03.java
@@ -0,0 +1,46 @@
+package recap.java_8.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class Stream03 {
+    public static void main(String[] args) {
+
+        //Verilen Liste
+        List<Match> matchList = Arrays.asList(
+                new Match("FootballMatch",11),
+                new Match("BasketballMatch",7),
+                new Match("Soccer",7),
+                new Match("Tennis",2)
+        );
+
+        Optional <String> matchName = matchList.stream()
+                .map(Match::getName)
+                .filter(name-> name.endsWith("Match"))   //Match ile bitenleri alacak
+                .sorted()                                //sıralayacak
+                .findFirst();                            //ilk sıradaki BasketBallMatch 'i yazacak
+
+        System.out.println("Result : "+ matchName.orElse("match no found"));
+
+
+    }
+
+}
+class Match {
+    public Match(String name, int playerCount) {
+        this.name = name;
+        this.playerCount = playerCount;
+    }
+
+    private String name;
+    private int playerCount;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPlayerCount() {
+        return playerCount;
+    }
+}
