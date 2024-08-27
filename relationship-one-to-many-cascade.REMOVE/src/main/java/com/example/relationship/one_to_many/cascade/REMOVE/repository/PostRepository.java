f897121ea2759e9cd361aa49d7fa01c9669df84c
Omerrizabahadir <omrbahadir@gmail.com>
2024-08-27 17:58:41 +0300

    deleted

--- relationship-one-to-many-cascade.REMOVE/src/main/java/com/example/relationship/one_to_many/cascade/REMOVE/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.relationship.one_to_many.cascade.REMOVE.repository;
-
-import com.example.relationship.one_to_many.cascade.REMOVE.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
